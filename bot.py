79564c90b33d6fbe3a534e03f21e22f0eab81a29
eric <eferrer23@gmail.com>
2017-05-03 02:16:15 -0700

    new stuff

---
+++ bot.py
@@ -0,0 +1,14 @@
+import tweepy
+from secrets import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+auth.set_access_token(access_token, access_secret)
+
+#create api instance
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
