b641f414a6754135e018f4c4b813a39e2d6f1228
eric <eferrer23@gmail.com>
2017-05-03 15:32:14 -0700

    calling api correctly

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
+import requests
+import random
+import time
 import tweepy
 from secrets import *
+
+#url = "http://numbersapi.com/58"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
@@ -9,6 +14,21 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+#public_tweets = api.home_timeline()
+#for tweet in public_tweets:
+#    print(tweet.text)
+
+def randomNum():
+
+
+def getFacts():
+    try:
+        r = requests.get('http://numbersapi.com/58')
+        print r.text
+        #return r.json()
+    except IOError:
+        print "error"
+
+randomNum()
+
+getFacts()
