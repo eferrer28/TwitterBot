919edb04e4118a978b3e051e72a96448036fe991
eric <eferrer23@gmail.com>
2017-05-03 23:06:01 -0700

    fully works & hiding stuff

--- cron.py
+++ cron.py
@@ -1,16 +1,16 @@
  #!/usr/bin/env python
 
-
+from secrets import *
 from crontab import CronTab
 
 cron = CronTab(user=True)
 
-job = cron.new(command='python /home/eferrer/Projects/TwitterBot/bot.py')
+job = cron.new(command=path)
 #job = cron.new(command='python /bot.py')
 
 #job  = cron.new(command='/usr/bin/echo')
 job.hour.every(4)
-#job.hour.on(12)
+#job.minute.every(3)
 job.enable()
 
 
