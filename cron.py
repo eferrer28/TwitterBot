0014475898782e074cecbc246832bb9c6ffc35c4
eric <eferrer23@gmail.com>
2017-05-03 23:00:45 -0700

    crom working

--- cron.py
+++ cron.py
@@ -1,9 +1,20 @@
+ #!/usr/bin/env python
+
+
 from crontab import CronTab
 
 cron = CronTab(user=True)
 
-job = cron.new(command='python /Projects/TwitterBot/bot.py')
-job.minute.every(15)
+job = cron.new(command='python /home/eferrer/Projects/TwitterBot/bot.py')
+#job = cron.new(command='python /bot.py')
+
+#job  = cron.new(command='/usr/bin/echo')
+job.hour.every(4)
 #job.hour.on(12)
+job.enable()
+
 
 cron.write()
+
+if cron.render():
+    print cron.render()
